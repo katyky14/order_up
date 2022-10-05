ce635698c0a8bb01afa2321773806bc2b8b1b44e
TonyTan <tonytan2193@gmail.com>
2022-10-05 14:31:51 -0700

    Finished up to CREATE A DATABASE MANAGEMENT SCRIPT

---
+++ order_up2/app/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from .config import Configuration
+from .models import db   # New import
+from .routes import orders, session
+from flask_login import LoginManager, current_user
+from .models import db, Employee
+
+
+app = Flask(__name__)
+app.config.from_object(Configuration)
+app.register_blueprint(orders.bp)
+app.register_blueprint(session.bp)
+db.init_app(app)  # Configure the application with SQLAlchemy
+
+login = LoginManager(app)
+login.login_view = "session.login"
+
+
+@login.user_loader
+def load_user(id):
+    return Employee.query.get(id)
